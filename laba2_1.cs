9d45c8141f777510577bf29e1ac9e0f30a9929a5
“NEEEEEERO” <onder.orkhan@yandex.ru>
2022-02-19 13:16:13 +0500

    Laba_21

---
+++ laba2_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace LAB_2_ISIT_ONDER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[\w\.\-]+@[\w]+\.[a-z]+\b";
+            string lab;
+            string line = @"C:\Users\Krast\Desktop\reg_vir\task1.txt";
+            StreamReader sr = new StreamReader(line);
+            Regex regex = new Regex(pattern); //шаблон(маска/паттерн)
+            string text = sr.ReadToEnd();
+            MatchCollection match = regex.Matches(text);  //ищет все вхождения - "Matches"
+            lab = match.Count.ToString();
+            Console.WriteLine(lab);
+        }
+    }
+}
